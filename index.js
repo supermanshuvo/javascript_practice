c34619f27c86d1ad12d9c6690e1aa5eac1952419
supermanshuvo <shuvothesuperman@gmail.com>
2022-06-07 23:27:55 +0600

    comment index.js file for new code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-setTimeout(()=>{
-    console.log('Shuvo love programming');
-},2000);
-
-console.log(__dirname);
-console.log(__filename);
+// setTimeout(()=>{
+//     console.log('Shuvo love programming');
+// },2000);
+//
+// console.log(__dirname);
+// console.log(__filename);
