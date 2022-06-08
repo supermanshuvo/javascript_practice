1fca31998123a7a5c241b7ac36ea4b5860c9ec51
supermanshuvo <shuvothesuperman@gmail.com>
2022-06-08 16:29:27 +0600

    Fat array

--- index.js
+++ index.js
@@ -4,3 +4,20 @@
 //
 // console.log(__dirname);
 // console.log(__filename);
+// let number =(a,b)=> a+b;
+// console.log(number(5,10));
+
+let javascript = {
+    name:'JavaScript',
+    libraries:["React","Angular","Vue"],
+    printLibraries: function (){
+        // let self = this;
+        // this.libraries.forEach(function (a){
+        //    console.log(`${self.name} love ${a}`);
+        // });
+        this.libraries.forEach((a)=>{
+            console.log(`${this.name} love ${a}`);
+        });
+    },
+};
+javascript.printLibraries();
