26b0e6afcc7ec8126242c317107486ce4a479a9b
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 20:35:34 +0600

    Logger check function

--- middleware/index.js
+++ middleware/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+const myLogger = function (req,res,next){
+    console.log('Logged');
+    next();
+};
+
+
 app.get('/',(req,res)=>{
     res.send('This is runnig in middleware file');
 });
-
-
 
 
 
