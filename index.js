d3aa8f66eee902135a33303b04239df3e63ff0ff
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 13:24:47 +0600

    index post request is create

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 app.get('/',(req,res)=>{
     res.send('shuvo is learning node js');
-})
+});
+app.post('/',(req,res)=>{
+   res.send('This is post request');
+});
 app.listen(3000,()=>{
     console.log('Running on Port:'+3000);
-})
+});
