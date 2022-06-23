876a764dea1934c2f2407e2cc704700f74d28845
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 21:00:41 +0600

    request time

--- middleware/index.js
+++ middleware/index.js
@@ -14,6 +14,18 @@ app.get('/',(req,res)=>{
 
 */
 
+const requestTime = function (req,res,next){
+    res.requestTime = Date.now();
+    next();
+};
+app.use(requestTime)
+
+app.get('/', (req, res) => {
+    let responseText = 'Hello World!<br>'
+    responseText += `<small>Requested at: ${req.requestTime}</small>`
+    res.send(responseText)
+})
+
 
 
 app.listen(port,function (req,res){
