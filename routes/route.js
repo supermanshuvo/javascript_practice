62fe29b0ebea2f14d1d371ede9b9699fc8614d0d
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 15:55:37 +0600

    app is now listen in port number 3000

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
-
+const port = 3000;
 /**
  * app get request
  */
 app.get('/',(req,res)=>{
     res.send('Root');
+});
+
+/**
+ * Running server port in 3000
+ */
+app.listen(port,()=>{
+   console.log(`Server is running on Port: ${port}`);
 });
