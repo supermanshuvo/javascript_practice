be01ac5ec595d0f5c7eab472ee92227473efe110
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 20:09:39 +0600

    middleware that is specific to the router is creat

--- routes/birds.js
+++ routes/birds.js
@@ -1,3 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * Middleware that is specific to this router
+ */
+router.use((req,res,next)=>{
+    console.log('Time ',Date.now());
+    next();
+});
