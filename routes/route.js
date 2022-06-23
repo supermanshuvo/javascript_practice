cbbe851799915d3d43072647d2c844747466ed05
Rezaul Karim <shuvothesuperman@gmail.com>
2022-06-23 15:52:25 +0600

    get Request create is done

--- routes/route.js
+++ routes/route.js
@@ -1,2 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+
+/**
+ * app get request
+ */
+app.get('/',(req,res)=>{
+    res.send('Root');
+});
